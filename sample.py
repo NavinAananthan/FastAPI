c56852b8a4fe7a161883501c97f5bc2ea9e483b3
NavinAananthan <navinaananthan63@gmail.com>
2023-02-18 10:46:18 +0530

    4

--- sample.py
+++ sample.py
@@ -2,7 +2,23 @@ from fastapi import FastAPI
 
 app=FastAPI()
 
+
+# Sample students data
+students={
+    1:{
+        "name":"navin",
+        "age":"20",
+        "year":"3rd"
+    }
+}
+
+
 # This is for the get request
 @app.get("/")
-def home():
+def index():
     return {"Hello": "world"}
+
+# This is to get the details of the students when id is given
+@app.get("/get-student/{student_id}")
+def get_student(student_id: int):
+    return students[student_id]
